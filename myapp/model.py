03057d46902198dee0213239facbfabfaf26006c
redstarht <185753202+redstarht@users.noreply.github.com>
2025-05-22 17:23:18 +0900

    20250522_comp

--- myapp/model.py
+++ myapp/model.py
@@ -1,2 +1,17 @@
 from .extension import db
 from datetime import datetime, timezone, timedelta
+
+class Product_number(db.Model):
+    __tablename__ = 'product_number'
+    id = db.Column(db.Integer, primary_key=True)
+    product_number = db.Column(db.String(50), unique=True, nullable=False)
+    is_deleted = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(
+        timezone(timedelta(hours=9))))
+    updated_at = db.Column(
+        db.DateTime, default=lambda: datetime.now(
+            timezone(timedelta(hours=9))),
+        onupdate=lambda: datetime.now(timezone(timedelta(hours=9)))
+
+    def __repr__(self):
+        return f'<Product_number {self.product_number}>'
