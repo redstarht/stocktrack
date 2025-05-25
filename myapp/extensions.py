9ccebc671c869009436b3be5a3ac6326a2699fa9
redstarht <185753202+redstarht@users.noreply.github.com>
2025-05-25 14:34:05 +0900

    20250525house

--- myapp/extensions.py
+++ myapp/extensions.py
@@ -1,4 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_migrate import Migratepip 
+from sqlalchemy import event
+import sqlite3
+from sqlalchemy.engine import Engine
+
+@evenvt.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, sqlite3.Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
+
 db = SQLAlchemy()
 migrate = Migrate()
--- myapp/model.py
+++ myapp/model.py
@@ -3,7 +3,7 @@ from datetime import datetime, timezone, timedelta
 
 class ProductNumber(db.Model):
     __tablename__ = 'product_number'
-    pn_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     product_number = db.Column(db.String(50), unique=True, nullable=False)
     is_deleted = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(
@@ -15,31 +15,34 @@ class ProductNumber(db.Model):
 
 class Shelf(db.Model):
     __tablename__ = 'shelf'
-    shelf_id = db.Column(db.Integer, primary_key=True)
-    shelf_name = db.Column(db.String(50), unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
 
 
 class Cell(db.Model):
     __tablename__ = 'cell'
-    cell_id = db.Column(db.Integer, primary_key=True)
-    cell_name = db.Column(db.String(50), unique=True, nullable=False)
-    shelf_id = db.Column(db.Integer, db.ForeignKey('shelf.shelf_id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    shelf_id = db.Column(db.Integer, db.ForeignKey('shelf.id'), nullable=False)
     is_all_pn_allowed = db.Column(db.Boolean, default=False)
+    Shelf = db.relationship('Shelf', backref="cells" lazy=True)  # cell.shelf.name \ shelf.cells
+   
 
 class AllowStorage(db.Model):
     __tablename__ = 'allow_storage'
-    cell_id = db.Column(db.Integer, db.ForeignKey('cell.cell_id'), primary_key=True,nullable=False)
-    pn_id = db.Column(db.Integer, db.ForeignKey('product_number.pn_id'), primary_key=True, nullable=False)
+    cell_id = db.Column(db.Integer, db.ForeignKey('cell.id'), primary_key=True,nullable=False)
+    pn_id = db.Column(db.Integer, db.ForeignKey('product_number.id'), primary_key=True, nullable=False)
+    cell = db.relationship('Cell', backref="allow_storage", lazy=True)  # cell.allow_storage
+    product_number = db.relationship('ProductNumber', backref="allow_storage", lazy=True)  # product_number.allow_storage
+
 
 class InoutLog(db.Model):
     __tablename__ = 'inout_log'
-    cell_id = db.Column(db.Integer, db.ForeignKey('cell.cell_id'), primary_key=True, nullable=False)
-    pn_id = db.Column(db.Integer, db.ForeignKey('product_number.pn_id'), primary_key=True, nullable=False)
-
-class InoutLog(db.Model):
-    __tablename__ = 'inout_log'
-    cell_id = db.Column(db.Integer, db.ForeignKey('cell.cell_id'), primary_key=True, nullable=False)
-    pn_id = db.Column(db.Integer, db.ForeignKey('product_number.pn_id'), primary_key=True, nullable=False)
-    inout_type = db.Column(db.Boolean, default=False, nullable=False)  # True: In, False: Out
+    id = db.Column(db.Integer, primary_key=True)
+    cell_id = db.Column(db.Integer, db.ForeignKey('cell.id'), primary_key=True, nullable=False)
+    pn_id = db.Column(db.Integer, db.ForeignKey('product_number.id'), primary_key=True, nullable=False)
+    inout_type = db.Column(db.String(10), default=False, nullable=False)  # True: In, False: Out
     processed_at = db.Column(db.DateTime, default=lambda: datetime.now(
         timezone(timedelta(hours=9))), nullable=False)
+    cell = db.relationship('Cell', backref="inout_log", lazy=True)  # cell.inout_log
+    product_number = db.relationship('ProductNUmber',backref="inout_log",lazy=True)  # product_number.inout_log
