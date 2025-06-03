d816c167ad054f8c8ef85d087b031f6a11aa22e5
redstarht <185753202+redstarht@users.noreply.github.com>
2025-06-03 18:47:38 +0900

    20250603

--- myapp/static/js/stock_ctrl.js
+++ myapp/static/js/stock_ctrl.js
@@ -5,6 +5,9 @@ const blocks = document.querySelectorAll('.gauge-block');
 const plusBtn = document.getElementById('plus-btn');
 const minusBtn = document.getElementById('minus-btn');
 
+// 保管上限数をpopupに表示
+document.getElementById('pop-stock-max').textContent = maxBlocks;
+
 plusBtn.addEventListener('click', () => {
   if (currentBlocks < maxBlocks) {
     blocks[currentBlocks].classList.add('active');
