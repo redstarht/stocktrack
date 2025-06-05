027896ece6ca8721dc6bd7a3f383c9e929539538
redstarht <185753202+redstarht@users.noreply.github.com>
2025-06-06 01:09:39 +0900

    20250606house

--- myapp/static/js/zone_sort.js
+++ myapp/static/js/zone_sort.js
@@ -1,8 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // すべての品番ボタンを取得
+    // すべてのゾーンを取得
     const zoneBtns = document.querySelectorAll(".zone-item");
+    // すべての品番ボタンを取得
+    const pnBtns = document.querySelectorAll(".pn-item");
+    
     // すべての棚を取得
     const shelves = document.querySelectorAll(".shelf");
+    const allzone = document.querySelector(".allzone");
+    // 全ての棚を表示するボタン
+    allzone.addEventListener("click", () => {
+        shelves.forEach(shelf => {
+            shelf.style.display = "block"; // 全ての棚を表示
+        });
+        // 全ての品番リストを表示
+        pnBtns.forEach(pnBtn => {
+            pnBtn.style.display = "block"; // 全ての品番リストを表示
+        });
+    });
 
     zoneBtns.forEach(zoneBtn => {
         zoneBtn.addEventListener("click", () => {
@@ -14,6 +28,16 @@ document.addEventListener("DOMContentLoaded", function () {
                     shelf.style.display = "none"; // 非表示
                 }
             });
+            // ゾーンに存在する品番リストの表示
+            pnBtns.forEach(pnBtn => {
+                const pnZone = pnBtn.dataset.zone;
+                if (pnZone === zoneId) {
+                    pnBtn.style.display = "block"; // 表示
+                } else {
+                    pnBtn.style.display = "none"; // 非表示
+                }
+            });
+
 
         });
     });
