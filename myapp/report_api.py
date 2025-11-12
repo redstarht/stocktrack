81660dae59cb8a0f7e8784f541e015f61db7ef8c
unknown <na-taniguchi@sango.co.jp>
2025-11-12 17:26:25 +0900

    20251112comp

--- myapp/report_api.py
+++ myapp/report_api.py
@@ -19,6 +19,7 @@ import datetime
 
 from logging import getLogger
 from sqlalchemy import desc, between
+from werkzeug.exceptions import BadRequest
 
 logger = getLogger()
 
@@ -27,47 +28,86 @@ v1 = Blueprint("v1", __name__, url_prefix='/api/v1')
 
 @v1.route('/zone')
 def order_zone():
-    obj_zones = Zone.query.all()
-    zones = [zone.to_dict() for zone in obj_zones]
-    return jsonify(zones)
+    try:
+        obj_zones = Zone.query.all()
+        zones = [zone.to_dict() for zone in obj_zones]
+        return jsonify({"success": True, "data": zones})
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "error_code": 500,
+            "message": f"ERROR: {e}"
+        })
 
 
 @v1.route('/shelf')
 def order_shelf():
-    obj_shelfs = Shelf.query.all()
-    shelfs = [shelf.to_api_dict() for shelf in obj_shelfs]
-    return jsonify(shelfs)
+    try:
+        obj_shelfs = Shelf.query.all()
+        shelfs = [shelf.to_api_dict() for shelf in obj_shelfs]
+        return jsonify({"success": True, "data": shelfs})
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "error_code": 500,
+            "message": f"ERROR: {e}"
+        })
 
 
 @v1.route('/cell')
 def order_cell():
-    obj_cells = Cell.query.all()
-    cells = [cell.to_api_dict() for cell in obj_cells]
-    return jsonify(cells)
+    try:
+        obj_cells = Cell.query.all()
+        cells = [cell.to_api_dict() for cell in obj_cells]
+        return jsonify({"success": True, "data": cells})
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "error_code": 500,
+            "message": f"ERROR: {e}"
+        })
 
 
 @v1.route('/product_number')
 def order_product_number():
-    obj_product_numbers = ProductNumber.query.all()
-    product_numbers = [pn.to_api_dict() for pn in obj_product_numbers]
-    return jsonify(product_numbers)
+    try:
+        obj_product_numbers = ProductNumber.query.all()
+        product_numbers = [pn.to_api_dict() for pn in obj_product_numbers]
+        return jsonify({"success": True, "data": product_numbers})
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "error_code": 500,
+            "message": f"ERROR: {e}"
+        })
 
 
 @v1.route("/cell_stock_status")
 # cell_stock_statusの値を、
 # 更新時間を描画するため、今の時間を返す
 def order_cell_stock_status():
-    obj_cell_stock_status = CellStockStatus.query.all()
-    cell_stock_statuses = [cell_stock_status.to_api_dict()
-                           for cell_stock_status in obj_cell_stock_status]
-    return jsonify(cell_stock_statuses)
+    try:
+        obj_cell_stock_status = CellStockStatus.query.all()
+        cell_stock_statuses = [cell_stock_status.to_api_dict()
+                            for cell_stock_status in obj_cell_stock_status]
+        return jsonify({"success": True, "data": cell_stock_statuses})
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "error_code": 500,
+            "message": f"ERROR: {e}"
+        })
+
 
 # http://127.0.0.1:5000/api/v1/inout_log
+
+
 @v1.route("/inout_log")
 def order_inout_log():
     str_year = request.args.get('year')
     obj_inout_logs = None
     inout_logs = []
+    your_request = None
 
     str_fromdate = request.args.get('fromdate')
     str_todate = request.args.get('todate')
@@ -78,44 +118,62 @@ def order_inout_log():
     dt_now = datetime.datetime.now()
 
     # InoutLog.query.order_by(desc(InoutLog.id)).limit(15000)
-    if (str_year):
-        start_year = datetime.datetime(int(str_year), 1, 1, 0, 0, 0, 0)
-        end_year = datetime.datetime(int(str_year), 12, 31, 12, 59, 59, 999999)
-        obj_inout_logs = InoutLog.query.filter(
-            InoutLog.processed_at.between(start_year, end_year)).all()
+    try:
+        if (str_year):
+            start_year = datetime.datetime(int(str_year), 1, 1, 0, 0, 0, 0)
+            end_year = datetime.datetime(
+                int(str_year), 12, 31, 12, 59, 59, 999999)
+            obj_inout_logs = InoutLog.query.filter(
+                InoutLog.processed_at.between(start_year, end_year)).all()
+            your_request = f"year={str_year},start={start_year},end={end_year}"
+
+        elif (fromdate is None and todate is None):
+            obj_inout_logs = InoutLog.query.order_by(
+                desc(InoutLog.id)).limit(15000)
+            your_request = f"fromdate is None and todate is None\n"
+
+        elif (fromdate and todate):
+            obj_inout_logs = InoutLog.query.filter(
+                InoutLog.processed_at.between(fromdate, todate)).limit(15000)
+            your_request = f"fromdate={fromdate},todate={todate}"
+
+        elif (fromdate and todate is None):
+            todate = dt_now
+            obj_inout_logs = InoutLog.query.filter(
+                InoutLog.processed_at.between(fromdate, todate)).limit(15000)
+            your_request = f"fromdate={fromdate},todate = None"
+
+        elif (fromdate is None and todate):
+            todate = dt_now
+            obj_inout_logs = InoutLog.query.filter(
+                InoutLog.processed_at >= fromdate).limit(15000)
+            your_request = f"fromdate = None,todate={todate}"
+
+        else:
+            raise BadRequest
+
         inout_logs = [inout_log.to_api_dict()
                       for inout_log in obj_inout_logs]
-        return inout_logs
+        dataToSend = {
+            "success": True,
+            "data": inout_logs
+        }
 
-    elif (fromdate is None and todate is None):
-        obj_inout_logs = InoutLog.query.order_by(
-            desc(InoutLog.id)).limit(15000)
-        inout_logs = [inout_log.to_api_dict()
-                      for inout_log in obj_inout_logs]
-        return inout_logs
+    except BadRequest as bre:
+        dataToSend = {
+            "success": False,
+            "error_code": 400,
+            "message": F"ERROR:{bre}",
+            "your_request": your_request
+        }
 
-    elif (fromdate and todate):
-        obj_inout_logs = InoutLog.query.filter(
-            InoutLog.processed_at.between(fromdate, todate)).limit(15000)
-        inout_logs = [inout_log.to_api_dict()
-                      for inout_log in obj_inout_logs]
-        return inout_logs
+    except Exception as e:
+        dataToSend = {
+            "success": False,
+            "error_code": 500,
+            "message": F"ERROR:{e}",
+            "your_request": your_request
+        }
 
-    elif (fromdate and todate is None):
-        todate = dt_now
-        obj_inout_logs = InoutLog.query.filter(
-            InoutLog.processed_at.between(fromdate, todate)).limit(15000)
-        inout_logs = [inout_log.to_api_dict()
-                      for inout_log in obj_inout_logs]
-        return inout_logs
-
-    elif (fromdate is None and todate):
-        todate = dt_now
-        obj_inout_logs = InoutLog.query.filter(
-            InoutLog.processed_at >= fromdate).limit(15000)
-        inout_logs = [inout_log.to_api_dict()
-                      for inout_log in obj_inout_logs]
-        return inout_logs
-
-    else:
-        raise ValueError
+    finally:
+        return jsonify(dataToSend)
